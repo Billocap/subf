94768b7e7372499f7c4979c4123d462bacb65cb7
Gabriel Quintino <pikachurando@protonmail.com>
2024-01-06 22:29:00 -0300

    ref: improved rollup config

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,4 +1,13 @@
-// rollup.config.js
+import path from "path";
+import { config } from "dotenv";
+import { expand } from "dotenv-expand";
+
+expand(config());
+
+import alias from "@rollup/plugin-alias";
+import commonjs from "@rollup/plugin-commonjs";
+import nodeResolve from "@rollup/plugin-node-resolve";
+import terser from "@rollup/plugin-terser";
 import typescript from "@rollup/plugin-typescript";
 
 export default {
@@ -7,5 +16,23 @@ export default {
     dir: "dist",
     format: "cjs",
   },
-  plugins: [typescript()],
+  plugins: [
+    typescript(),
+    alias({
+      entries: [
+        {
+          find: "@/",
+          replacement: path.resolve("src"),
+        },
+      ],
+    }),
+    nodeResolve(),
+    commonjs(),
+    terser({
+      format: {
+        comments: false,
+      },
+      compress: false,
+    }),
+  ],
 };
